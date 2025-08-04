85cde925e8948a573634e0f5e646d1b1fde9f0a7
ingprovencio <ingprovencio@gmail.com>
2025-08-04 20:18:32 +0200

    Hasta 2a

---
+++ emotion_detection.py
@@ -0,0 +1,16 @@
+
+import requests
+import json 
+def emotion_detector(text_in):
+    URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    HEADERS = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = { "raw_document": { "text": str(text_in) } }
+    try:
+        response = requests.post(URL, headers=HEADERS, json = input_json)
+        response_json = response.json()
+        return response.json['text']
+    except Exception as e:
+        print ('Error: ' + str(e))
+        return None
+
+#print (emotion_detector("Me encanta esta nueva tecnología"))
